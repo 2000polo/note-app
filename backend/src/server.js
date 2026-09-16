1777a0b3337b43f092b4cffe6b433f16f466dc2e
Arun Paul <apaul7445@gmail.com>
2026-09-16 16:27:04 +0530

    deployment configurations are done

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
@@ -11,11 +12,15 @@ import rateLimitter from './middleware/rateLimitter.js';
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-}))
+if(process.env.prod !== "production"){
+  app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }))
+}
+
 // middleware
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +29,14 @@ app.use(rateLimitter);
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/notes', notesRoutes);
+
+if(process.env.NODE_ENV == "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("*", (re, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
 
 const startServer = async () => {
   try {
